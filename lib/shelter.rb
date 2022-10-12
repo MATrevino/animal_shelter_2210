f1089057f3f0868bbc08218ee0c4f7ed3c38b621
Meredith Trevino <meredithtrevino@gmail.com>
2022-10-12 09:44:21 -0600

    completed I1

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,13 +1,9 @@
 class Shelter
-attr_reader :name, :capacity
+attr_reader :name, :capacity, :pets
 
-def initialize(name, capacity, pets)
+def initialize(name, capacity)
   @name = name
   @capacity = capacity
   @pets = []
 end
-end
-
-def pets
-  []
 end
